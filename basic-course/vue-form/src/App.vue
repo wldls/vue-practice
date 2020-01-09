6010aaaf960a3fd6c2b698fdc4c6419ecea6674d
jiin <dslet2343@gmail.com>
2020-01-09 17:14:01 +0900

    vue.js 기초강의 완강

---
+++ basic-course/vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data:function(){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods:{
+    submitForm:function(){
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      console.log(this.username, this.password);
+
+      axios.post(url, data)
+        .then(function(res){
+          console.log(res);          
+        })
+        .catch(function(err){
+          console.log(err);          
+        });      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
