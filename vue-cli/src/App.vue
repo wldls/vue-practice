32dbfe3ba793b7f73466a56dbc65cc92f3ddf83b
jiin <dslet2343@gmail.com>
2020-01-09 15:51:16 +0900

    ~ 싱글 파일 컴포넌트

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:props 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data:function(){
+    return {
+      str:'Header'
+    }
+  },
+  components:{
+    'app-header': AppHeader
+  },
+  methods:{
+    renewStr:function(){
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
