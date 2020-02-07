1342877a3437134120b83613d82326c5bf83efa6
jiin <dslet2343@gmail.com>
2020-02-07 19:49:32 +0900

    차트 적용

---
+++ vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Chart.js</h2>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+
+export default {
+  // 컴포넌트 속성 & 인스턴스 옵션
+  components:{
+    BarChart,
+    LineChart,
+  }
+}
+</script>
+
+<style>
+
+</style>
