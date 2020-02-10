671f15687e054179a074c6eb1f67a1c7865ee3b8
jiin <dslet2343@gmail.com>
2020-02-10 15:53:49 +0900

    차트 플러그인 제작 및 모듈화 실습

---
+++ vue-charts/src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<canvas ref="barChart" id="barChart" width="400" height="400"></canvas>	  
+	</div>
+</template>
+
+<script>
+// chart 컴포넌트 모듈화
+// import Chart from 'chart.js';
+
+export default {
+	props:['propsdata'],
+	mounted(){
+			// var ctx = document.getElementById('barChart');
+			const myChart = new this.$_Chart(this.$refs.barChart, {
+					type: 'bar',
+					data: {
+							labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+							datasets: this.propsdata
+					},
+					options: {
+							scales: {
+									yAxes: [{
+											ticks: {
+													beginAtZero: true
+											}
+									}]
+							}
+					}
+			});
+
+		console.log(myChart);
+	}
+}
+</script>
+
+<style>
+
+</style>
--- vue-charts/src/components/LineChart.vue
+++ vue-charts/src/components/LineChart.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-import Chart from 'chart.js';
+// import Chart from 'chart.js';
 
 export default {
 	mounted(){
 		// var ctx = document.getElementById('lineChart').getContext('2d');
-		const chart = new Chart(this.$refs.lineChart, {
+		const chart = new this.$_Chart(this.$refs.lineChart, {
 				// The type of chart we want to create
 				type: 'line',
 
